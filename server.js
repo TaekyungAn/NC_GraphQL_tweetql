091a01a7aee6c96825cc0053a6e0d7402401e0cd
Taekyung An <atk0426@naver.com>
2022-08-02 21:42:52 +0900

    Mutation Type

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ const typeDefs = gql`
     allTweets: [Tweet]
     tweet(id: ID): Tweet
   }
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    deleteTweet(id: ID): Boolean
+  }
 `;
+
+// GET /api/v1/tweets
+// POST DELETE PUT/api/v1/tweets
+// GET /api/v1/tweet/:id
+
 const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
